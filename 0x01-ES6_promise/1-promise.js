a903e32bc0cba27a8e2003e91c56edb2d88ed01d
David Joy <davidjoy.win@gmail.com>
2023-06-30 01:41:30 +0100

    task done for today

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,14 @@
+export default function getFullResponseFromAPI(success) {
+  return new Promise ((resolved, rejected) => {
+    if (success) {
+      resolved({
+        'status': 200,
+        'body': 'Success'
+      })
+    }
+
+    else {
+      rejected(Error("The fake API is not working currently"));
+    }
+  })
+}
